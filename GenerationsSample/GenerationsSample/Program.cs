b91e792bf4151b84d4519868dc32f97b1ecf0e84
Christian Nagel <christian@christiannagel.com>
2018-07-03 17:15:49 +0200

    GC generations

---
+++ GenerationsSample/GenerationsSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerationsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generations();
+        }
+
+        private static void Generations()
+        {
+            var o1 = new object();
+            int g1 = GC.GetGeneration(o1);
+            Console.WriteLine(g1);
+            GC.Collect();
+            g1 = GC.GetGeneration(o1);
+            Console.WriteLine(g1);
+            GC.Collect();
+            g1 = GC.GetGeneration(o1);
+            Console.WriteLine(g1);
+            GC.Collect();
+            g1 = GC.GetGeneration(o1);
+            Console.WriteLine(g1);
+            GC.Collect();
+            g1 = GC.GetGeneration(o1);
+            Console.WriteLine(g1);
+        }
+    }
+}
