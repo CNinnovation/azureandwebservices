88bf8537b02f3fdb6760922fa4b9556d1f3ea743
Christian Nagel <christian@christiannagel.com>
2018-07-03 17:15:59 +0200

    WCF sample

---
+++ MyWCFServiceLib/MyWCFServiceLib/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWCFServiceLib
+{
+    [DataContract]
+    public class Book
+    {
+        [DataMember]
+        public int BookId { get; set; }
+
+        [DataMember]
+        private string _title;
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        [DataMember]
+        public string Publisher { get; set; }
+    }
+}
---
+++ MyWCFServiceLib/MyWCFServiceLib/BooksService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyWCFServiceLib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class BooksService : IBooksService
+    {
+        public Book GetBook(int bookId)
+        {
+            return new Book { BookId = 1, Title = "Professional C# 7", Publisher = "Wrox Press" };
+        }
+    }
+}
